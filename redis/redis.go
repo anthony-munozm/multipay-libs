ce5c7289f9602a14af32eb81c4a530865439e703
Kervin5705 <kervin5705@gmail.com>
2025-09-18 08:37:31 -0600

    main

---
+++ redis/redis.go
@@ -0,0 +1,138 @@
+package utils
+
+import (
+  "encoding/json"
+  "fmt"
+  "time"
+  "github.com/redis/go-redis/v9"
+  "context"
+  "log"
+  "github.com/anthony-munozm/multipay-libs/bridge"
+)
+
+var Rdb *redis.Client
+
+type UniversalCache struct {
+  Key string `json:"key"`
+  Data interface{} `json:"data"`
+  Expiration time.Duration `json:"expiration"`
+}
+
+func NewRedisClient(redisURL string, password string, db int) *redis.Client {
+    rdb := redis.NewClient(&redis.Options{
+        Addr:     redisURL,
+        Password: password,
+        DB:       db,
+    })
+    Rdb = rdb
+    return Rdb
+}
+
+func SaveSettingsList(rdb *redis.Client, microservice string, configs []map[string]interface{}, ttl time.Duration) {
+    key := fmt.Sprintf("config:%s", microservice)
+
+    jsonData, err := json.Marshal(configs)
+    if err != nil {
+      log.Println("Error creando lista", err)
+    }
+
+    if ttl == 0 {
+        ttl = time.Hour
+    }
+
+    err = rdb.Set(context.Background(), key, jsonData, ttl).Err()
+    if err != nil {
+      log.Println("Error guardando lista", err)
+    }
+
+    log.Println("Lista de configuraciones guardada")
+}
+
+func SaveUniversalCache(rdb *redis.Client, key string, data interface{}, ttl time.Duration) {
+  jsonData, err := json.Marshal(data)
+  if err != nil {
+    log.Println("Error creando lista", err)
+  }
+
+  if ttl == 0 {
+    ttl = time.Hour
+  }
+
+  err = rdb.Set(context.Background(), key, jsonData, ttl).Err()
+  if err != nil {
+    log.Println("Error guardando lista", err)
+  }
+
+  log.Println("Lista de configuraciones guardada")
+}
+
+func GetUniversalCacheTyped[T any](rdb *redis.Client, key string) (T, error) {
+  var result T
+  val, err := rdb.Get(context.Background(), key).Result()
+  if err != nil {
+    return result, err
+  }
+
+  err = json.Unmarshal([]byte(val), &result)
+  if err != nil {
+    return result, fmt.Errorf("error unmarshaling cached data: %w", err)
+  }
+
+  return result, nil
+}
+
+func GetSettingsList(rdb *redis.Client, microservice string, settingsVersion string) []map[string]interface{} {
+    key := fmt.Sprintf("config:%s", microservice)
+
+    val, err := rdb.Get(context.Background(), key).Result()
+    if err != nil {
+        if err == redis.Nil {
+          log.Println("No existe configuración")
+            return nil
+        }
+        log.Println("Error leyendo lista", err)
+        return nil
+    }
+
+    var configs []map[string]interface{}
+    err = json.Unmarshal([]byte(val), &configs)
+    if err != nil {
+      log.Println("Error parseando lista", err)
+      return nil
+    }
+
+    cleanConfigs := []map[string]interface{}{}
+
+    for _, config := range configs {
+      version, exists := config["version"];
+      if exists {
+        versionStr := fmt.Sprintf("%v", version)
+        if versionStr == settingsVersion { 
+          log.Println("Versiones iguales")
+          cleanConfigs = append(cleanConfigs, config)
+        }
+      }
+    }
+
+    return cleanConfigs
+}
+
+func UpdateSettingsList(rdb *redis.Client, microservice string, ttl time.Duration) { 
+  settings := bridge.NewMicroserviceClient().CallAdminCore("GET", "/settings", nil, nil)
+  if settingsMap, ok := settings.(map[string]interface{}); ok {
+    if success, exists := settingsMap["success"]; exists && success == true {
+      key := fmt.Sprintf("config:%s", microservice)
+      jsonData, err := json.Marshal(settingsMap["response"])
+      if err != nil {
+        log.Println("Error creando lista", err)
+      }
+      if ttl == 0 {
+        ttl = time.Hour
+      }
+      err = rdb.Set(context.Background(), key, jsonData, ttl).Err()
+      if err != nil {
+        log.Println("Error guardando lista", err)
+      }
+    }
+  }
+}
