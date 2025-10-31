d5a5c7bf8e4d9a86f662b6a13703fd0f3432ad10
Kervin5705 <kervin5705@gmail.com>
2025-10-31 07:35:32 -0600

    mainm

--- utilslib/utilslib.go
+++ utilslib/utilslib.go
@@ -4,6 +4,10 @@ import (
 	"github.com/anthony-munozm/multipay-libs/redis"
 	"github.com/anthony-munozm/multipay-libs/errormap"
 	"log"
+	"strings"
+	"errors"
+	"strconv"
+	"math"
 )
 
 func GetDecimalsFromCurrency(currency string) (int, error, string) {
@@ -38,4 +42,39 @@ func GetDecimalsFromCurrency(currency string) (int, error, string) {
         return 0, errormap.GenerateErrorMessage("PAYMENT_INVALID_REQUEST", "currency not found in catalog"), "PAYMENT_INVALID_REQUEST"
     }
     return decimals, nil, ""
+}
+
+
+func NormalizeAmount(amount string, decimals int) (string, error) {
+	if strings.HasSuffix(amount, ".") {
+		return "", errors.New("INVALID_AMOUNT_FORMAT")
+	}
+
+	if strings.Count(amount, ".") > 1 {
+		return "", errors.New("INVALID_AMOUNT_FORMAT")
+	}
+
+	value, err := strconv.ParseFloat(amount, 64)
+	if err != nil {
+		return "", errors.New("INVALID_AMOUNT_FORMAT")
+	}
+
+	if value <= 0 {
+		return "", errors.New("INVALID_AMOUNT")
+	}
+
+	integerValue := int64(value * math.Pow10(decimals))
+	
+	return strconv.FormatInt(integerValue, 10), nil
+}
+
+func NormalizeAmountReverse(amount string, decimals int) (string, error) {
+	value, err := strconv.ParseFloat(amount, 64)
+	if err != nil {
+		return "", errors.New("INVALID_AMOUNT_FORMAT")
+	}
+
+	integerValue := float64(value / math.Pow10(decimals))
+	
+	return strconv.FormatFloat(integerValue, 'f', decimals, 64), nil
 }
